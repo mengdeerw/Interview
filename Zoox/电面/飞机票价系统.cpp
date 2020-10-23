42d04164c2f0751012da1b520e2230cf3547611f
mengdeerw <41224329+mengdeerw@users.noreply.github.com>
2020-10-22 19:45:43 -0700

    Update 飞机票价系统.cpp

--- Zoox/电面/飞机票价系统.cpp
+++ Zoox/电面/飞机票价系统.cpp
@@ -49,160 +49,201 @@ Explanation of Output:
 */
 
 #include <string>
+#include <iostream>
 #include <sstream>
 #include <algorithm>
 #include <iterator>
 #include <map>
 #include <vector>
+#include <assert.h>
 
 using namespace std;
 namespace Airfee{
-enum Seat{ eco, pre, bus};
-enum Airline{ delta, united, southwest};
-map<string, Airline> airlines {{"delta",delta}, {"united",united}, {"southwest",southwest}};
-map<string, Seat> seats {{"economy",eco}, {"premium",pre}, {"business",bus}};
+	enum Seat { eco, pre, bus};
+	enum Airline { delta, united, southwest};
+	map<string, Airline> airlines {
+		{"delta", delta}, 
+		{"united", united}, 
+		{"southwest", southwest}};
+	map<string, Seat> seats {
+		{"economy", eco}, 
+		{"premium", pre}, 
+		{"business", bus}};
+}
 
 class AirlineCalculator{
 public:
-// AirlineCalculator() = default;
-static AirlineCalculator* create(Airline air);
-virtual float calcFee(Seat s, float dis) = 0;
-virtual ~AirlineCalculator() = default;
+	// IMP!! Pay attention to static keyword
+	static AirlineCalculator* create(Airfee::Airline air);
+	virtual float calcFee(Airfee::Seat s, float dis) = 0;
+	virtual ~AirlineCalculator() = default;
 protected:
-virtual float getOpCost (Seat s, float dis) {
-float opCost = 0.;
-switch(s)
+	virtual float getOpCost (Airfee::Seat s, float dis) 
+	{
+		float opCost = 0.;
+		switch(s)
+		{
+			case(Airfee::eco):
+				opCost = getEcoOpCost(dis);
+				break;
+			case(Airfee::pre):
+				opCost = getPreOpCost(dis);
+				break;
+			case(Airfee::bus):
+				opCost = getBusOpCost(dis);
+		}
+		return opCost;
+	}
+	virtual float getEcoOpCost (float d) 
+	{
+		return 0.;
+	}
+	virtual float getPreOpCost (float d) 
+	{
+		return 25.;
+	}
+	virtual float getBusOpCost (float d) 
+	{
+		return 50.+ 0.25 * d;
+	}
+};
+
+class DeltaCalculator: public AirlineCalculator{
+public:
+	float calcFee(Airfee::Seat s,float dis) override 
+	{
+		float opCost = getOpCost(s,dis);
+		return opCost + dis * 0.5;
+	}
+	virtual ~DeltaCalculator() = default;
+	static AirlineCalculator* instance()
+	{
+		static DeltaCalculator calc;
+		return &calc;
+	}
+// make original constructor private, so that object cannot be created directly
+private:
+	DeltaCalculator() = default;
+};
+
+class UnitedCalculator: public AirlineCalculator{
+public:
+	float calcFee(Airfee::Seat s,float dis) override 
+	{
+		float opCost = getOpCost(s,dis);
+		return opCost + dis * 0.75;
+	}
+	virtual ~UnitedCalculator() = default;
+	static AirlineCalculator* instance()
+	{
+		static UnitedCalculator calc;
+		return &calc;
+	}
+private:
+	UnitedCalculator() = default;
+protected:
+	float getPreOpCost(float d) override
+	{
+		return 25. + 0.1 * d;
+	}
+};
+
+class SouthwestCalculator: public AirlineCalculator{
+public:
+	virtual ~SouthwestCalculator() = default;
+	float calcFee(Airfee::Seat s,float dis) override
+	{
+		return 1. * dis;
+	}
+	static AirlineCalculator* instance()
+	{
+		static SouthwestCalculator calc;
+		return &calc;
+	}
+private:
+	SouthwestCalculator() = default;
+};
+
+// Factory method to create objects of different airline calculators
+// the essence of the Factory Method i.e. private constructor & static method.
+AirlineCalculator* AirlineCalculator::create(Airfee::Airline air)
 {
-case(eco):
-opCost = getEcoOpCost(dis);
-break;
-case(pre):
-opCost = getPreOpCost(dis);
-break;
-case(bus):
-opCost = getBusOpCost(dis);
+	switch(air) 
+	{
+		case Airfee::delta:
+			return DeltaCalculator::instance();
+		case Airfee::united:
+			return UnitedCalculator::instance();
+		case Airfee::southwest:
+			return SouthwestCalculator::instance();
+		default:
+			return NULL;
+	}
 }
-return opCost;
-}
-virtual float getEcoOpCost (float d) {
-return 0.;
-}
-virtual float getPreOpCost (float d) {
-return 25.;
-}
-virtual float getBusOpCost (float d) {
-return 50.+ 0.25 * d;
-}
-};
-
-class DeltaCalculator:public AirlineCalculator{
-public:
-float calcFee(Seat s,float dis) override {
-float opCost = getOpCost(s,dis);
-return opCost + dis * 0.5;
-}
-virtual ~DeltaCalculator() = default;
-static AirlineCalculator* instance(){
-static DeltaCalculator calc;
-return &calc;
-}
-private:
-DeltaCalculator() = default;
-};
-
-class UnitedCalculator:public AirlineCalculator{
-public:
-float calcFee(Seat s,float dis) override {
-float opCost = getOpCost(s,dis);
-return opCost + dis * 0.75;
-}
-virtual ~UnitedCalculator() = default;
-static AirlineCalculator* instance(){
-static UnitedCalculator calc;
-return &calc;
-}
-private:
-UnitedCalculator() = default;
-protected:
-float getPreOpCost(float d) override{
-return 25. + 0.1 * d;
-}
-};
-
-class SouthwestCalculator:public AirlineCalculator{
-public:
-virtual ~SouthwestCalculator() = default;
-float calcFee(Seat s,float dis) override{
-return 1. * dis;
-}
-static AirlineCalculator* instance(){
-static SouthwestCalculator calc;
-return &calc;
-}
-private:
-SouthwestCalculator() = default;
-};
-
-
-
-AirlineCalculator* AirlineCalculator::create(Airline air){
-switch(air) {
-case delta:
-return DeltaCalculator::instance();
-case united:
-return UnitedCalculator::instance();
-case southwest:
-return SouthwestCalculator::instance();
-}
-}
-
 
 vector<string> split_tolower(string s, char delim = ' ');
-vector<string> split_tolower(string s, char delim){
-vector<string> res;
-std::transform(s.begin(), s.end(), s.begin(), ::tolower);
-std::stringstream ss(s);
-std::string token;
-while (std::getline(ss, token, delim) ) {
-if(token != "\0")
-res.push_back(token);
-}
-return res;
+vector<string> split_tolower(string s, char delim)
+{
+	vector<string> res;
+	std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+	std::stringstream ss(s);
+	std::string token;
+	while (std::getline(ss, token, delim)) 
+	{
+		if(token != "\0")
+			res.push_back(token);
+	}
+	return res;
 }
 
 //map<string, AirlineCalculator*> airclcs{{"delta",DeltaCalculator::instance()}, {"united",UnitedCalculator::instance()}, {"southwest",SouthwestCalculator::instance()}};
 
-vector<float> processData(vector<string> ticketData){
-vector<float> res;
-for(auto &ticket:ticketData){
-vector<string> data = split_tolower(ticket);
+vector<float> processData(vector<string> ticketData)
+{
+	vector<float> res;
+	for(auto &ticket: ticketData)
+	{
+		vector<string> data = split_tolower(ticket);
 
-assert(data.size() == 3);
-assert(airlines.count(data[0]));
-assert(seats.count(data[2]));
-float dis;
-try{
-dis = stof(data[1]); //100a.0->100.0
-} catch(const char *exception){
-cout << exception;
-throw;
-}
+		assert(data.size() == 3);
+		assert(Airfee::airlines.count(data[0]));
+		assert(Airfee::seats.count(data[2]));
+		float dis;
+		try
+		{
+			dis = stof(data[1]); //100a.0->100.0
+		}
+		catch (const char *exception)
+		{
+			cout << exception;
+			throw;
+		}
 
-AirlineCalculator* clc = AirlineCalculator::create(airlines[data[0]]);
-//AirlineCalculator* clc = airclcs[data[0]];
-cout<< data[0] << "add: "<<clc<<endl;
-res.push_back(clc->calcFee(seats[data[2]], dis));
-
-}
-return res;
-}
+		AirlineCalculator* clc = AirlineCalculator::create(Airfee::airlines[data[0]]);
+		//AirlineCalculator* clc = airclcs[data[0]];
+		//cout<< data[0] << " add: "<<clc<<endl;
+		res.push_back(clc->calcFee(Airfee::seats[data[2]], dis));
+	}
+	return res;
 }
 
 
 int main() {
-vector<string> input{"United 150.0 Premium", "United 120.0 economy","United 100.0 business","Delta 60.0 economy","Delta 60.0 premium","Delta 60.0 Business", "SouthWest 1000.0 Economy", "SouthWest 4000.0 Economy"};
-vector<float> costs = Airfee::processData(input);
-for(int i = 0 ; i < input.size(); i++){
-cout<< input<<" cost: $"<<costs<<endl;
+	vector<string> input {
+		"United 150.0 Premium", 
+		"United 120.0 economy",
+		"United 100.0 business",
+		"Delta 60.0 economy",
+		"Delta 60.0 premium",
+		"Delta 60.0 Business", 
+		"SouthWest 1000.0 Economy", 
+		"SouthWest 4000.0 Economy"
+	};
+	vector<float> costs = processData(input);
+	for(int i = 0 ; i < input.size(); i++)
+	{
+		//cout<< input[i] <<" cost: $" << costs[i] << endl;
+		cout << costs[i] << endl;
+	}
+	return 0;
 }
-return 0;

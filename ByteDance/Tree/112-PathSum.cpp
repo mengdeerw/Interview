534cc7aa85d95a154b0e764428018b63c6a51f79
mengdeerw <41224329+mengdeerw@users.noreply.github.com>
2020-11-30 22:06:43 -0800

    Create 112-PathSum.cpp

---
+++ ByteDance/Tree/112-PathSum.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+//dfs
+//root-to-leaf path: choose either left or right subtree
+//递归到左子树或者右子树时不要忘记扣除当前节点的值
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (!root) return false;
+        if (!root->left && !root->right && root->val == sum) return true;
+        return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val); 
+    }
+};

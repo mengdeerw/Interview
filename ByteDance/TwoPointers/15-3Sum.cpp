bdd79610aa7b91efaf906d4d51c553a35c0ee226
mengdeerw <41224329+mengdeerw@users.noreply.github.com>
2020-12-03 20:24:34 -0800

    Create 15-3Sum.cpp

---
+++ ByteDance/TwoPointers/15-3Sum.cpp
@@ -0,0 +1,31 @@
+// Time complexity: O(N^2)
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        if (nums.size() < 3) return result;
+        
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int left = i + 1, right = nums.size() - 1;
+            while (left < right) {
+                int target = -nums[i];
+                if (nums[left] + nums[right] == target) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } else if (nums[left] + nums[right] < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+         }
+        return result;
+    }
+};

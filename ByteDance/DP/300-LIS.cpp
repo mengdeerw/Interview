0c3b34e41b0f174bbbd13c1f3a9aab8ff7cf9f27
mengdeerw <41224329+mengdeerw@users.noreply.github.com>
2020-12-30 10:34:38 -0800

    Update 300-LIS.cpp

--- ByteDance/DP/300-LIS.cpp
+++ ByteDance/DP/300-LIS.cpp
@@ -20,3 +20,34 @@ public:
         return result;
     }
 };
+
+// Binary Search (Patience sorting)
+// Time complexity: O(NlogN)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> top (nums.size(), 0);
+        int piles = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int poker = nums[i];
+            int left = 0, right = piles;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (top[mid] > poker) {
+                    right = mid;
+                } else if (top[mid] < poker) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            
+            // 新建牌堆
+            if (left == piles) piles++;
+            top[left] = poker;
+        }
+        
+        return piles;
+    }
+};

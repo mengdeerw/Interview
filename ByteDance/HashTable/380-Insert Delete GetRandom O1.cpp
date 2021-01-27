3b82b8f54eb12935998cefad856276eb24e1f02d
mengdeerw <41224329+mengdeerw@users.noreply.github.com>
2021-01-26 22:55:10 -0800

    Update 380-Insert Delete GetRandom O1.cpp

--- ByteDance/HashTable/380-Insert Delete GetRandom O1.cpp
+++ ByteDance/HashTable/380-Insert Delete GetRandom O1.cpp
@@ -1,3 +1,6 @@
+// follow up question: 
+// https://leetcode.com/problems/insert-delete-getrandom-o1-duplicates-allowed/
+
 class RandomizedSet {
 public:
     /** Initialize your data structure here. */

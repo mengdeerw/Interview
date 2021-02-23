0aedc4317a48a11f1af5b48ba4515cc4b044e119
mengdeerw <41224329+mengdeerw@users.noreply.github.com>
2021-02-22 21:00:45 -0800

    Update 269-Alien Dictionary.cpp

--- ByteDance/Graph/269-Alien Dictionary.cpp
+++ ByteDance/Graph/269-Alien Dictionary.cpp
@@ -57,3 +57,62 @@ public:
         return result.length() == indegree.size() ? result : "";
     }
 };
+
+// DFS
+class Solution {
+public:
+    unordered_map<char, vector<char>> graph;
+    unordered_map<char, bool> visited;
+    string result;
+    
+    string alienOrder(vector<string>& words) {
+        //step 1: initialize
+        for (string word: words) {
+            for (char c: word) {
+                graph[c] = {};
+            }
+        }
+        
+        //step 2: build graph
+        for (int i = 0; i < words.size() - 1; i++) {
+            string word1 = words[i];
+            string word2 = words[i + 1];
+            int len = min(word1.length(), word2.length()); 
+            for (int j = 0; j < len; j++) {
+                if (word1[j] != word2[j]) {
+                    graph[word1[j]].push_back(word2[j]);
+                    break;
+                }
+            }
+            
+            if (word1.find(word2) == 0 && word1.length() > word2.length()) return "";
+        }
+        
+        // step 3: DFS
+        for (auto kv: graph) {
+            char c = kv.first;
+            bool noCycle = dfs(c);
+            if (!noCycle) return "";
+        }
+        
+        if (result.length() < graph.size()) return "";
+        
+        reverse(result.begin(), result.end());
+        return result;
+    }
+    
+    // return true if no cycles
+    bool dfs(char c) {
+        if (visited.find(c) != visited.end()) {
+            return visited[c];
+        }
+        visited[c] = false;
+        for (char next: graph[c]) {
+            bool noCycle = dfs(next);
+            if (!noCycle) return false;
+        }
+        visited[c] = true;
+        result += c;
+        return true;
+    }
+};

3ffa424f54f4a933d59ecd56e72812d597ad1fd5
mengdeerw <41224329+mengdeerw@users.noreply.github.com>
2021-02-23 22:08:10 -0800

    Create 394-Decode Strings.cpp

---
+++ ByteDance/DFS/394-Decode Strings.cpp
@@ -0,0 +1,71 @@
+// METHOD-1: using 1 stack
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] != ']') {
+                st.push(s[i]);
+            } else {
+                string decodeString = "";
+                while (st.top() != '[') {
+                    decodeString += st.top();
+                    st.pop();
+                }
+                // pop [ from stack
+                st.pop();
+                int base = 1, times = 0;
+                while (!st.empty() && isdigit(st.top()) {
+                    times = times + (st.top() - '0') * base;
+                    st.pop();
+                    base *= 10;
+                }
+                while (times != 0) {
+                    for (int j = decodeString.length() - 1; j >= 0; j--) {
+                        st.push(decodeString[j]);
+                    }
+                    times--;
+                }
+            }
+        }
+        
+        string result;
+        for (int i = st.size() - 1; i >= 0; i--) {
+            result = st.top() + result;
+            st.pop();
+        }
+        return result;
+    }
+};
+                       
+// METHOD-2: Using 2 stacks
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> countSt;
+        stack<string> stringSt;
+        string str;
+        int k = 0;
+        for (char c: s) {
+            if (isdigit(c)) {
+                k = k * 10 + c - '0';
+            } else if (c == '[') {
+                countSt.push(k);
+                stringSt.push(str);
+                str = ""; // reset str
+                k = 0;
+            } else if (c == ']') {
+                string decodeString = stringSt.top();
+                stringSt.pop();
+                for (int i = countSt.top() - 1; i >= 0; i--) {
+                    decodeString += str;
+                }
+                countSt.pop();
+                str = decodeString;
+            } else {
+                str += c;
+            }
+        }
+        return str;
+    }
+};

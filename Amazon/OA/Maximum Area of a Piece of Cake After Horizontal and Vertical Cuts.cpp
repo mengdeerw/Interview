620198185d155ef9097859323f17302b9689960f
mengdeerw <41224329+mengdeerw@users.noreply.github.com>
2021-04-06 20:44:17 -0700

    Update Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts.cpp

--- Amazon/OA/Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts.cpp
+++ Amazon/OA/Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts.cpp
@@ -1,6 +1,7 @@
 // leetcode 1465
 // https://leetcode.com/problems/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/
 
+// time complexity: O(NlogN)
 // pay attention to large int number
 class Solution {
 public:
@@ -23,4 +24,21 @@ public:
         max = std::max(max, limit - last);
         return max;
     }
+};
+
+// another version
+class Solution {
+public:
+  int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
+    constexpr int kMod = 1e9 + 7;
+    sort(begin(verticalCuts), end(verticalCuts));
+    sort(begin(horizontalCuts), end(horizontalCuts));
+    int mx = max(verticalCuts[0], w - verticalCuts.back());
+    int my = max(horizontalCuts[0], h - horizontalCuts.back());
+    for (int i = 1; i < verticalCuts.size(); ++i)
+      mx = max(mx, verticalCuts[i] - verticalCuts[i - 1]);
+    for (int i = 1; i < horizontalCuts.size(); ++i)
+      my = max(my, horizontalCuts[i] - horizontalCuts[i - 1]);               
+    return static_cast<long>(mx) * my % kMod;
+  }
 };

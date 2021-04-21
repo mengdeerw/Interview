1ef12c7798ddd06307e694bdb5aba8a834694798
mengdeerw <41224329+mengdeerw@users.noreply.github.com>
2021-04-20 22:14:58 -0700

    Create 973-KClosestPointstoOrigin.cpp

---
+++ Amazon/Leetcode/973-KClosestPointstoOrigin.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        // max heap
+        auto cmp = [](auto& a, auto& b){
+            return a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1];
+        };
+        priority_queue<vector<int>, vector<vector<int>>, decltype(cmp)> pq(cmp);
+        for (auto point: points) {
+            pq.push(point);
+            if (pq.size() > k) pq.pop();
+        }
+        vector<vector<int>> result;
+        while (!pq.empty()) {
+            result.push_back(pq.top());
+            pq.pop();
+        }
+        return result;
+    }
+};

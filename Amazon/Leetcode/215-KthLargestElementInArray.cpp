aa29ecef2daebeb1273f7422c12dc1748105f4f7
mengdeerw <41224329+mengdeerw@users.noreply.github.com>
2021-04-20 20:26:09 -0700

    Create 215-KthLargestElementInArray.cpp

---
+++ Amazon/Leetcode/215-KthLargestElementInArray.cpp
@@ -0,0 +1,67 @@
+// min heap
+// time complexity: O(NlogK)
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        // maintain a min heap with fixed size of k
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int num: nums) {
+            pq.push(num);
+            if (pq.size() > k) pq.pop();
+        }
+        return pq.top();
+    }
+};
+
+// max heap
+// time complexity: O(NlogN)
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        // max heap
+        priority_queue<int, vector<int>, less<int>> pq;
+        for (int num: nums) {
+            pq.push(num);
+        }
+        while (--k) {
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
+
+// quick sort
+// time complexity: average O(n), worst case O(n^2)
+class Solution {
+public:
+    // quick sort: elements>=pivot | pivot | elements<=pivot
+    int findKthLargest(vector<int>& nums, int k) { //average O(n), worst case O(n2)
+        if(k<1 || k>nums.size()) return -1;
+        int start = 0, end = nums.size()-1;
+        int pivot, l, r;
+        while(1){        
+            pivot = nums[start]; l = start; r = end; //set the pivot and checking window
+
+            while(l <= r){
+                if(nums[l] >= pivot) l++;
+                else if(nums[r] <= pivot) r--;
+                else {swap(nums[l], nums[r]);}
+            }
+            swap(nums[start], nums[r]); //pivot is fixed at index r, which is (r+1)th 
+
+            if(k == r+1) return nums[r];
+            else if(k > r+1) start = r+1; //move to pivot's right part
+            else {end = r-1;} //move to pivot's left part
+        }
+    }
+};
+
+// STL sort
+// time complexity: O(NlogN)
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() - k];
+    }
+};

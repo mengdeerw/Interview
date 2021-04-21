aa4e9b8ea1936c61baa45bcd23c0dd7c20ba91fb
mengdeerw <41224329+mengdeerw@users.noreply.github.com>
2021-04-20 20:37:16 -0700

    Update 215-KthLargestElementInArray.cpp

--- Amazon/Leetcode/215-KthLargestElementInArray.cpp
+++ Amazon/Leetcode/215-KthLargestElementInArray.cpp
@@ -56,6 +56,39 @@ public:
     }
 };
 
+// another version: quick sort
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int left=0, right=nums.size()-1;
+        while(true){
+            /*** return pivot position ***/
+            int pos=help(nums, left, right);
+            /** found position **/
+            if(pos==k-1)  return nums[pos];
+            /** the bigger contains more number **/
+            if(pos>k-1)  right=pos-1;
+            /** the smaller contains more number **/
+            else left=pos+1;
+        }
+    }
+    
+    /*** quick-sort-based-pivot-function: return-the-position-of-the-left-element-in-the-partitioned-list ***/
+    int help(vector<int>& nums, int left, int right){
+        int pivot=nums[left];
+        int l=left+1, r=right;
+        while(l<=r){
+            /*** move the bigger number left and smaller number right ***/
+            if(nums[l]<pivot && nums[r]>pivot)   swap(nums[l++], nums[r--]);
+            if(nums[l]>=pivot)  l++;
+            if(nums[r]<=pivot)  r--;
+        }
+        /*** swap the pivot ***/
+        swap(nums[left], nums[r]);
+        return r;
+    }
+};
+
 // STL sort
 // time complexity: O(NlogN)
 class Solution {

96723f69236ca4cac7f4ca50532588b7bc2a2347
mengdeerw <41224329+mengdeerw@users.noreply.github.com>
2021-04-30 00:01:56 -0700

    Update 42-TrappingRainWater.cpp

--- Amazon/Leetcode/42-TrappingRainWater.cpp
+++ Amazon/Leetcode/42-TrappingRainWater.cpp
@@ -30,7 +30,40 @@ public:
     }
 };
 
+// two pointers
+// Time complexity: O(N)
+// Space complexity: O(1)
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        if (height.empty()) return 0;
+        int result = 0; 
+        int len = height.size();  
+        int left = 0, right = len - 1;
+        int left_max = 0, right_max = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= left_max) {
+                    left_max = height[left];
+                } else {
+                    result += left_max - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= right_max) {
+                    right_max = height[right];
+                } else {
+                    result += right_max - height[right];
+                }
+                right--;
+            }
+        }
+        return result;
+    }
+};
+
 // Brute force
+// time complexity: O(N^2)
 class Solution {
 public:
     int trap(vector<int>& height) {

9fc4e4c668a3566719d9467b79e71ffcb1db30a8
mengdeerw <41224329+mengdeerw@users.noreply.github.com>
2021-05-02 20:06:24 -0700

    Update 763-PartitionLabels.cpp

--- Amazon/Leetcode/763-PartitionLabels.cpp
+++ Amazon/Leetcode/763-PartitionLabels.cpp
@@ -30,8 +30,9 @@ class Solution {
 public:
     vector<int> partitionLabels(string S) {
       vector<int> ans;
-      int start = 0, end = 0;
-      for (int i = 0; i < S.size(); ++i) {
+      size_t start = 0;
+      size_t end = 0;
+      for (size_t i = 0; i < S.size(); ++i) {
         end = max(end, S.find_last_of(S[i]));
         if (i == end) {
           ans.push_back(end - start + 1);

8f6616fdf35d96182e898b71d8c975a2c1e1640c
mengdeerw <41224329+mengdeerw@users.noreply.github.com>
2021-06-06 16:35:17 -0700

    Update 236-1644-LCA.cpp

--- Facebook/236-1644-LCA.cpp
+++ Facebook/236-1644-LCA.cpp
@@ -8,7 +8,7 @@
  * };
  */
 
-// leetcode-236
+// LEETCODE 236: LCAi
 // DFS: the idea is to recursively check curr node, its left & right child to see if there is any nodes equal to target p and q;
 /* 
 1. traverse to the left leaf node and right leaf node;
@@ -30,7 +30,7 @@ public:
     }
 };
 
-// leetcode 1644
+// LEETCODE 1644: LCAii
 // the difference from leetcode-236 is that we need to traverse the whole tree to see if both p and q are in the tree
 class Solution {
 public:
@@ -60,5 +60,71 @@ private:
         
         if (left && right) return root;
         return left ? left : right;
+    }
+};
+
+// LEETCODE 1650: LCAiii
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* parent;
+};
+*/
+
+/* METHOD-1: 
+1. Calculate the depth of both nodes
+2. move the nodes to their parents and update the depth until the depths are the same
+3. while parent nodes are not pointing to the same node, contiuously visit their parents until they are the same.
+*/
+// time complexity: O(h) where h is the height of the treee
+class Solution {
+public:
+    Node* lowestCommonAncestor(Node* p, Node * q) {
+        int p_depth = depth(p), q_depth = depth(q);
+        
+        while (p_depth > q_depth) {
+            p_depth--;
+            p = p->parent;
+        }
+        
+        while (q_depth > p_depth) {
+            q_depth--;
+            q = q->parent;
+        }
+        // at this point, q_depth == p_depth, two nodes are at the same level;
+        while (p != q) {
+            p = p->parent;
+            q = q->parent;
+        }
+        return p;
+    }
+    
+    int depth(Node* node) {
+        int depth = 0;
+        while (node) {
+            node = node->parent;
+            depth++;
+        }
+        return depth;
+    }
+};
+
+// METHOD2: 
+//It is just a variation of Intersection of LinkedList!
+// Time complexity: O(h1 + h2)
+class Solution {
+public:
+    Node* lowestCommonAncestor(Node* p, Node * q) {
+        Node* pp = p;
+        Node* qq = q;
+        while (pp != qq) {
+            pp = (pp == NULL) ? q : pp->parent;
+            qq = (qq == NULL) ? p : qq->parent;
+        }
+        return pp;
     }
 };

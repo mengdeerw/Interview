3e78ea3ef04eefa2f8b51384d837f8c6265d24e5
mengdeerw <41224329+mengdeerw@users.noreply.github.com>
2021-06-07 10:36:47 -0700

    Update 340-LongestSubstringAtMostKChars.cpp

--- Facebook/340-LongestSubstringAtMostKChars.cpp
+++ Facebook/340-LongestSubstringAtMostKChars.cpp
@@ -1,4 +1,4 @@
-// sliding window:
+// METHOD-1: sliding window
 // maintain a sliding window that only contains at most k distinct chars
 // time complexity: O(N)
 class Solution {
@@ -22,6 +22,41 @@ public:
                 }
             }
             max_len = max(max_len, right - left);
+        }
+        return max_len;
+    }
+};
+
+// METHOD-2: sliding window
+// two pointers with a variable to count distinct char inside the window
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        if (k < 1) return 0;
+        vector<int> freq (128, 0);
+        int left = 0, right = 0;
+        int max_len = 0;
+        int num_distinct = 0;
+        while (right < s.length()) {
+            char c = s[right];
+            freq[c]++;
+            right++;
+            if (freq[c] == 1) { // new char
+                num_distinct++;
+            }
+            if (num_distinct <= k) {
+                // update max length
+                max_len = max(max_len, right - left);
+            }
+            while (num_distinct > k) { // need to shrink window
+                char d = s[left]; 
+                freq[d]--;
+                left++;
+                if (freq[d] == 0) {
+                    num_distinct--;
+                }
+            }
+
         }
         return max_len;
     }

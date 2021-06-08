5208a3eb6948aaf13172666dd6c8099355a90288
mengdeerw <41224329+mengdeerw@users.noreply.github.com>
2021-06-07 20:32:22 -0700

    Update 53-MaximumSubarray.cpp

--- Facebook/53-MaximumSubarray.cpp
+++ Facebook/53-MaximumSubarray.cpp
@@ -1,4 +1,4 @@
-// brute force
+// METHOD-1: brute force
 // time complexity: O(N^2)
 class Solution {
 public:
@@ -17,7 +17,7 @@ public:
     }
 };
 
-// dp
+// METHOD-2: dp
 // time complexity: O(N)
 // space complexity: O(N)
 class Solution {
@@ -34,7 +34,7 @@ public:
     }
 };
 
-// dp
+// METHOD-3: dp
 // time complexity: O(N)
 // space complexity: O(1)
 class Solution {
@@ -48,5 +48,58 @@ public:
             max_sum = max(max_sum, cur_sum);
         }
         return max_sum;
+    }
+};
+
+// METHOD-4: Divide and conquer
+// time complexity: O(NlogN)
+// space complexity: O(1)
+class Solution {
+public:
+    vector<int> numsArray;
+    
+    int maxSubArray(vector<int>& nums) {
+        numsArray = nums;
+        
+        // Our helper function is designed to solve this problem for
+        // any array - so just call it using the entire input!
+        return findBestSubarray(0, numsArray.size() - 1);
+    }
+    
+private:
+    int findBestSubarray(int left, int right) {
+        // Base case - empty array.
+        if (left > right) {
+            return INT_MIN;
+        }
+        
+        int mid = left + (right - left) / 2;
+        int curr = 0;
+        int bestLeftSum = 0;
+        int bestRightSum = 0;
+        
+        // Iterate from the middle to the beginning.
+        for (int i = mid - 1; i >= left; i--) {
+            curr += numsArray[i];
+            bestLeftSum = max(bestLeftSum, curr);
+        }
+        
+        // Reset curr and iterate from the middle to the end.
+        curr = 0;
+        for (int i = mid + 1; i <= right; i++) {
+            curr += numsArray[i];
+            bestRightSum = max(bestRightSum, curr);
+        }
+        
+        // The bestCombinedSum uses the middle element and the best
+        // possible sum from each half.
+        int bestCombinedSum = numsArray[mid] + bestLeftSum + bestRightSum;
+        
+        // Find the best subarray possible from both halves.
+        int leftHalf = findBestSubarray(left, mid - 1);
+        int rightHalf = findBestSubarray(mid + 1, right);
+        
+        // The largest of the 3 is the answer for any given input array.
+        return max(bestCombinedSum, max(leftHalf, rightHalf));
     }
 };

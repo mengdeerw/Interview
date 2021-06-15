e317d19d97d35ee292b39957e9b95622ec1fc4ea
mengdeerw <41224329+mengdeerw@users.noreply.github.com>
2021-06-14 23:18:54 -0700

    Update 358-RearrangeStringKDistanceApart.cpp

--- Facebook/358-RearrangeStringKDistanceApart.cpp
+++ Facebook/358-RearrangeStringKDistanceApart.cpp
@@ -1,36 +1,38 @@
+
+// hashmap + priority queue
 class Solution {
 public:
     string rearrangeString(string s, int k) {
         if (k == 0) return s;
         
-        unordered_map<char, int> m;
-        for (char c: s) {
-            m[c]++;
+        unordered_map<char, int> freq;
+        for (char& c: s) {
+            freq[c]++;
         }
         
-        priority_queue<pair<int, char>> pq; // max heap {count, char}
-        for (auto& kv: m) {
-            pq.push(make_pair(kv.second, kv.first));
+        priority_queue<pair<int, char>> pq; // max heap with default comparator {freq, char}
+        for (auto& kv: freq) {
+            pq.push({kv.second, kv.first});
         }
         
-        string result;
+        string result = "";
         int len = s.length();
         while (!pq.empty()) {
-            vector<pair<int, char>> cache;
-            int count = min(len, k);
+            vector<pair<int, char>> remain; // to save the remaining pairs
+            int count = min(len, k); // how many chars remained to be dealt with
             for (int i = 0; i < count; i++) {
-                if (pq.empty()) return "";
+                if (pq.empty()) return ""; // used up all available char, no possible solutions
                 auto pair = pq.top();
                 pq.pop();
-                result.push_back(pair.second);
-                pair.first--;
-                if (pair.first > 0) {
-                    cache.push_back(pair);
-                }
+                result.push_back(pair.second); // use the max frequency char
+                pair.first--; // update frequency
+                remain.push_back(pair);
                 len--;
             }
-            for (auto p: cache) {
-                pq.push(p);
+            
+            // update pq with cache
+            for (auto& pair: remain) {
+                if (pair.first > 0) pq.push(pair);
             }
         }
         return result;

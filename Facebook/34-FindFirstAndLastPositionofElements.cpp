b4f72dd168fb423bc3970fe93688470b97d008a0
mengdeerw <41224329+mengdeerw@users.noreply.github.com>
2021-07-15 10:26:06 -0700

    Update 34-FindFirstAndLastPositionofElements.cpp

--- Facebook/34-FindFirstAndLastPositionofElements.cpp
+++ Facebook/34-FindFirstAndLastPositionofElements.cpp
@@ -1,3 +1,4 @@
+// 模板一： while (left <= right)
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {;
@@ -41,5 +42,54 @@ private:
         // left = right + 1; check right
         if (right < 0 || nums[right] != target) return -1;
         return right;
+    }
+};
+
+// 模板二： while (left + 1 < right)
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {;
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return {first, last};
+    }
+
+private:
+    int findFirst(vector<int>& nums, int target) {
+        if (nums.size() == 0) return -1;
+        int left = 0, right = nums.size() - 1; // [l, r]
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) { // shrink right boundary
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        // left + 1 = right
+        if (nums[left] != target && nums[right] != target) return -1;
+        if (nums[left] == target) return left;
+        return right;
+    }
+    
+    int findLast(vector<int>& nums, int target) {
+        if (nums.size() == 0) return -1;
+        int left = 0, right = nums.size() - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) { // shrink left boundary
+                left = mid;
+            } else if (nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        // left + 1 = right
+        if (nums[left] != target && nums[right] != target) return -1;
+        if (nums[right] == target) return right;
+        return left;
     }
 };
